9aedb1ad236abe978e0e864690696c5fb91ea8a8
Bill Garate <william.c.garate@gmail.com>
2015-03-02 22:37:15 -0500

    Added some comments to vice.c
    Added some comments to vice.c

--- src/vice.c
+++ src/vice.c
@@ -1,4 +1,14 @@
-// vice.c
+/***************************************************************
+ *****Virtue, a chess playing engine derived from VICE 1.1******
+ ****Credit and thanks to BlueFeverSoft, developer of VICE******
+ *******************Developed by Bill Garate********************
+ ******************Licensed under MIT License*******************
+ ***************************************************************
+ * FILE NAME: virtue.c                                         *
+ * PURPOSE:                                                    *
+ * Play chess in various capacities                            *
+ *                                                             *
+ ***************************************************************/
 
 #include "stdio.h"
 #include "defs.h"
@@ -9,6 +19,7 @@
 #define WAC1 "r1b1k2r/ppppnppp/2n2q2/2b5/3NP3/2P1B3/PP3PPP/RN1QKB1R w KQkq - 0 1"
 #define PERFT "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"
 
+/* The main program loop */
 int main(int argc, char *argv[]) {
 
 	AllInit();
